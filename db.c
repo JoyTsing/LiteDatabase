bb09e088d779f453e52395d143e874cbef6632e1
joyTsing <792566033@qq.com>
2021-03-16 10:48:06 +0800

    ont node b-tree

--- db.c
+++ db.c
@@ -168,6 +168,17 @@ void leaf_node_insert(Cursor* cursor,uint32_t key,Row* value){
     serialize_row(value,leaf_node_value(node,cursor->cell_num));
 }
 
+void print_leaf_node(void* node){
+    uint32_t num_cells=*leaf_node_num_cells(node);
+    printf("leaf (size %d)\n",num_cells);
+    for(uint32_t i=0;i<num_cells;i++){
+        uint32_t key=*leaf_node_key(node,i);
+        printf("  - %d : %d\n",i,key);
+    }
+}
+
+
+
 
 Pager* pager_open(const char* filename){
     int fd=open(filename,
@@ -304,7 +315,12 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer,Table* table){
     if(strcmp(input_buffer->buffer,".exit")==0){
         db_close(table);
         exit(EXIT_SUCCESS);
-    }else if(strcmp(input_buffer->buffer,".constants")==0){
+    }else if(strcmp(input_buffer->buffer,".btree")==0){
+        printf("Tree:\n");
+        print_leaf_node(get_page(table->pager,0));
+        return META_COMMAND_SUCCESS;
+    }
+    else if(strcmp(input_buffer->buffer,".constants")==0){
         printf("Constants:\n");
         print_constants();
         return META_COMMAND_SUCCESS;
